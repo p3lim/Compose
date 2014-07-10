b7bfdb23be609319d13d3a9499d0b81bb8e9c6e9
Adrian L Lange <adrianlund@gmail.com>
2014-07-10 17:48:55 +0200

    Add a nice new icon by @jxnblk

--- Resources.Designer.cs
+++ Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace Compose {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Icon {
+            get {
+                object obj = ResourceManager.GetObject("Icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] MouseKeyboardActivityMonitor {
